97f67fc93e3ea9230e7a4e6cc2fe41f70433da32
Tay Chang Jing <changjingtay@gmail.com>
2024-07-30 01:06:06 +0800

    feat: materialized

--- temp/mv.sql
+++ temp/mv.sql
@@ -1,3 +1,6 @@
-CREATE MATERIALIZED VIEW `healthy-skill-393206.MtCO2_flat.mv_emmission` AS
+CREATE OR REPLACE MATERIALIZED VIEW `healthy-skill-393206.MtCO2_flat.mv_emmission` AS
 SELECT *
 FROM `healthy-skill-393206.MtCO2_flat.emmission`
+WHERE Total != 0.0
+
+DROP MATERIALIZED VIEW IF EXISTS `healthy-skill-393206.MtCO2_flat.mv_emmission`
