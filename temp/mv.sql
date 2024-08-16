9ed8ca471597a98029cc781182ff2806c2643644
Tay Chang Jing <changjingtay@gmail.com>
2024-08-16 10:40:36 +0800

    feat: clear

--- temp/mv.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE MATERIALIZED VIEW `healthy-skill-393206.MtCO2_flat.mv_emmission` AS
-SELECT *
-FROM `healthy-skill-393206.MtCO2_flat.emmission`
-WHERE Total != 0.0
-
-DROP MATERIALIZED VIEW IF EXISTS `healthy-skill-393206.MtCO2_flat.mv_emmission`
